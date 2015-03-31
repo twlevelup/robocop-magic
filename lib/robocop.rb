840fd8f999200e87508cc52e31001f70d2e18a80
gldsouza <dsouzagladwin@gmail.com>
2015-03-31 20:35:30 +1100

    Robot to turn left and right functionality

--- lib/robocop.rb
+++ lib/robocop.rb
@@ -1,5 +1,40 @@
 class Robocop
+ @direction
+
     def beep
         'beep boop'
     end
+		
+		# 
+		# Initialize the Robot, confirmed with the BA that 
+		# the robot would start at (0,0) facing north
+		#
+		def initialize 
+			@direction = 'N'
+		end
+		
+		def set_direction (direction)
+			@direction = direction
+		end
+		
+		def turn_left
+			case @direction
+				when 'N'
+					@direction = 'W' 
+				when 'W'
+					@direction = 'S'
+				when 'S'
+					@direction = 'E'
+				when 'E'
+					@direction = 'N'
+				else
+					'Error: Invalid direction'
+			end
+		end
+		
+		def turn_right
+			turn_left
+			turn_left
+			turn_left
+		end
 end
--- spec/robocop_spec.rb
+++ spec/robocop_spec.rb
@@ -5,4 +5,74 @@ RSpec.describe Robocop do
     d = Robocop.new
     expect(d.beep).to eq('beep boop')
   end
+	
+	it "it should face west after starting the robot" do
+		d = Robocop.new
+    expect(d.turn_left).to eq('W')
+	end
+	
+	it "it should face west when it turns left from north" do
+		d = Robocop.new
+		d.set_direction('N')
+    expect(d.turn_left).to eq('W')
+	end
+	
+	it "it should face south when it turns left from west" do
+		d = Robocop.new
+		d.set_direction('W')
+    expect(d.turn_left).to eq('S')
+	end
+	
+	it "it should face East when it turns left from South" do
+		d = Robocop.new
+		d.set_direction('S')
+    expect(d.turn_left).to eq('E')
+	end
+	
+	it "it should face North when it turns left from East" do
+		d = Robocop.new
+		d.set_direction('E')
+    expect(d.turn_left).to eq('N')
+	end
+	
+	it "It should show error if the direction is not [N,S,E,W]" do
+		d = Robocop.new
+		d.set_direction('X')
+    expect(d.turn_left).to eq('Error: Invalid direction')
+	end
+	
+	it "it should face East after starting the robot" do
+		d = Robocop.new
+    expect(d.turn_right).to eq('E')
+	end 
+	
+	it "it should face east when it turns right from north" do
+		d = Robocop.new
+		d.set_direction('N')
+    expect(d.turn_right).to eq('E')
+	end
+	
+	it "it should face south when it turns right from east" do
+		d = Robocop.new
+		d.set_direction('E')
+    expect(d.turn_right).to eq('S')
+	end
+	
+	it "it should face West when it turns right from South" do
+		d = Robocop.new
+		d.set_direction('S')
+    expect(d.turn_right).to eq('W')
+	end
+	
+	it "it should face North when it turns right from West" do
+		d = Robocop.new
+		d.set_direction('W')
+    expect(d.turn_right).to eq('N')
+	end
+	
+	it "It should show error if the direction is not [N,S,E,W]" do
+		d = Robocop.new
+		d.set_direction('X')
+    expect(d.turn_right).to eq('Error: Invalid direction')
+	end
 end
